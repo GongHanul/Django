7f88559bd328e79ac6984b54ecb86734f2b702de
gonoghanan@naver.com <gonoghanan@naver.com>
2023-03-03 17:21:08 +0900

    Fix file address

--- venvs/pjt_1_access.cmd
+++ venvs/pjt_1_access.cmd
@@ -1,3 +1,3 @@
 @echo off
-cd C:\S_PJT\projects\pjt_1
-C:\S_PJT\venvs\venv_1\Scripts\activate
+cd C:\Users\SSAFY\Desktop\S_PJT\django_pjt\projects\pjt_1
+C:\Users\SSAFY\Desktop\S_PJT\django_pjt\venvs\venv_1\Scripts\activate
